33713ea6b93693f3415d79b47d5f101a2639519b
Towfik <tufikhasan05@gmail.com>
2022-04-09 16:15:20 +0600

    Storing Data in2 Database Using Mongoose Model

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,16 @@
 const connectToMongo = require("./db");
 connectToMongo();
-
 const express = require("express");
 
 const app = express();
 const port = 5000;
+//Midleware to express deal with json file
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+//Available Routes
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Notebook-Backend listening at http://localhost:${port}`);
 });
---
+++ backend/routes/auth.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+
+router.post("/", (req, res) => {
+  console.log(req.body);
+  const user = User(req.body);
+  user.save();
+  res.send(req.body);
+});
+
+module.exports = router;
