87af8fa5b6d0225f6079feaaf21c397cac75936c
Towfik <tufikhasan05@gmail.com>
2022-04-11 14:56:05 +0600

    .......

--- backend/middleware/fetchuser.js
+++ backend/middleware/fetchuser.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const JWT_SECRET_KEY = process.env.REACT_APP_SECRET_KEY;
 
 const fetchuser = (req, res, next) => {
+  // Get the user from the jwt token and add id to req object
   const token = req.header("token");
   if (!token) {
     res.status(401).send({ error: "Please authenticate a valid token" });
