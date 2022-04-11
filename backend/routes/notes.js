d9b0aaeb368cf8cb21b7c5ddad483fd8992553ca
Towfik <tufikhasan05@gmail.com>
2022-04-11 15:52:37 +0600

    Updating an existing Notes endpoint completed

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -47,5 +47,44 @@ router.get("/fatchallnotes", fetchuser, async (req, res) => {
     res.status(500).send("Internal server error");
   }
 });
+//ROUTE 03: Add new note using: PUT "/api/notes/updatenote".login required
+router.put("/updatenote/:id", fetchuser, async (req, res) => {
+  const { title, description, tag } = req.body;
+  try {
+    //create a updateNote object
+    const updateNote = {};
+    //if title exist then update title
+    if (title) {
+      updateNote.title = title;
+    }
+    //if description exist then update description
+    if (description) {
+      updateNote.description = description;
+    }
+    //if tag exist then update tag
+    if (tag) {
+      updateNote.tag = tag;
+    }
+    //Find the note by id to be updated and update it
+    let note = await Notes.findById(req.params.id);
+    if (!note) {
+      return res.status(404).send("Not found");
+    }
+
+    if (note.user.toString() !== req.user.id) {
+      return res.status(401).send("Not allowed");
+    }
+    note = await Notes.findByIdAndUpdate(
+      req.params.id,
+      { $set: updateNote },
+      { new: true }
+    );
+
+    res.json({ note });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal server error");
+  }
+});
 
 module.exports = router;
