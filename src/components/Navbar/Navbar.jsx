1ee72a456d80df397bfc394c8f824d4edbb62d55
Towfik <tufikhasan05@gmail.com>
2022-04-13 02:57:38 +0600

    Navbar completed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.scss";
 import { Link } from "react-router-dom";
+import { BiAlignLeft } from "react-icons/bi";
+import { HiX } from "react-icons/hi";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
   return (
     <>
       <div className="app__navbar">
-        <div className="app__navbar-logo"></div>
-        <ul className="app__navbar-links">
-          {["home", "about"].map((item) => (
-            <li className="app__flex" key={`link-${item}`}>
-              <div />
-              <Link to={`/${item === "home" ? "" : item}`}>{item}</Link>
-            </li>
-          ))}
-        </ul>
-        <div className="app__navbar-sign-btns">
-          <button>Sign in</button>
-          <button>Sign in</button>
+        <div className="app__navbar-logo">
+          <h2>
+            <Link to={"/"}>iNotes</Link>
+          </h2>
+        </div>
+        <div className="app__navbar-navigation">
+          <ul className="app__navbar-links">
+            {["home", "about"].map((item) => (
+              <li className="app__flex" key={`link-${item}`}>
+                <div />
+                <Link to={`/${item === "home" ? "" : item}`}>{item}</Link>
+              </li>
+            ))}
+          </ul>
+          <div>
+            <button className="primary_button">Sign in</button>
+            <button className="primary_button">Sign up</button>
+          </div>
+        </div>
+        <div className="app__navbar-menu">
+          <BiAlignLeft
+            onClick={() => {
+              setToggle(true);
+            }}
+          />
+          {toggle && (
+            <div>
+              <HiX
+                onClick={() => {
+                  setToggle(false);
+                }}
+              />
+              <ul>
+                {["home", "about"].map((item) => (
+                  <li key={`link-${item}`}>
+                    <Link
+                      to={`/${item === "home" ? "" : item}`}
+                      onClick={() => {
+                        setToggle(false);
+                      }}
+                    >
+                      {item}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+              <span>
+                <button
+                  onClick={() => {
+                    setToggle(false);
+                  }}
+                  className="primary_button"
+                >
+                  Sign in
+                </button>
+                <button
+                  onClick={() => {
+                    setToggle(false);
+                  }}
+                  className="primary_button"
+                >
+                  Sign up
+                </button>
+              </span>
+            </div>
+          )}
         </div>
       </div>
     </>
