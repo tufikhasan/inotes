2a477e2501601c1cd05bdf2d8ad953ed2e91b9d7
Towfik <tufikhasan05@gmail.com>
2022-04-14 12:41:32 +0600

    noteitem design

--- src/components/NoteItem/NoteItem.jsx
+++ src/components/NoteItem/NoteItem.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./NoteItem.scss";
 
-const NoteItem = () => {
-  return <div>NoteItem</div>;
+const NoteItem = (props) => {
+  const { title, description, tag } = props;
+  return (
+    <div className="app__note">
+      <h3 className="bold-text">{title}</h3>
+      <p className="p-text">{description}</p>
+      <p className="p-text">{tag}</p>
+    </div>
+  );
 };
 
 export default NoteItem;
--- src/components/Notes/Notes.jsx
+++ src/components/Notes/Notes.jsx
@@ -5,7 +5,19 @@ import "./Notes.scss";
 const Notes = () => {
   return (
     <div className="app__notes">
-      <NoteItem />
+      <h2 className="head-text">
+        all <span>of Your</span> notes
+      </h2>
+      <div>
+        <NoteItem
+          title="title"
+          description="description jg kgj  kjg nfkg gg f g fng g gfdkg n gfnkg g ng ng"
+          tag="tag"
+        />
+        <NoteItem title="title" description="description" tag="tag" />
+        <NoteItem title="title" description="description" tag="tag" />
+        <NoteItem title="title" description="description" tag="tag" />
+      </div>
     </div>
   );
 };
